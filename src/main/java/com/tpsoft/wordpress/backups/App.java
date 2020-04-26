e7f7c8375295f1488310ac6b5a8829111867fc55
tpsumeta <tpsumeta@gmail.com>
2020-04-26 20:33:46 +0700

    ftp download file

--- src/main/java/com/tpsoft/wordpress/backups/App.java
+++ src/main/java/com/tpsoft/wordpress/backups/App.java
@@ -1,25 +1,33 @@
 package com.tpsoft.wordpress.backups;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * Hello world!
- *
- */
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
 public class App 
 {
+	
+	private static Configuration config = null;
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        config = new Configuration();     
         
-        createFile();
+        run();
         
     }
 
-	private static void createFile()
+	private static void run()
 	{
 		String pattern = "yyyyddMM-HHmmss";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
@@ -37,6 +45,7 @@ public class App
       	                String dateStr = dateFormat.format(new Date());
       	                writer.write("Backup start date "+ dateStr);
       	                writer.close();
+      	                ftp(directory);
       	            } else {
       	                System.out.println("Failed to create file!");
       	            }
@@ -44,6 +53,92 @@ public class App
               }
         }catch (Exception e) {
         	 System.out.println("Failed to create directories!");
+		}
+	}
+	
+
+	private static void ftp(File directory) {
+        try {
+            //new ftp client
+            FTPClient ftp = new FTPClient();
+            //try to connect
+            ftp.connect(config.getHost());
+            //login to server
+            if (!ftp.login(config.getUser(),config.getPass())) {
+                ftp.logout();
+            }
+            int reply = ftp.getReplyCode();
+            //FTPReply stores a set of constants for FTP reply codes. 
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                ftp.disconnect();
+            }
+            //enter passive mode
+            ftp.enterLocalPassiveMode();
+            //get system name
+            System.out.println("Remote system is " + ftp.getSystemType());
+            //change current directory
+            ftp.changeWorkingDirectory("/");
+            System.out.println("Current directory is " + ftp.printWorkingDirectory());
+
+            File sourcePath = new File(directory + "\\source\\"); 
+            if (sourcePath.mkdirs()){
+            	downloadAllFile(ftp,"/",sourcePath.toString());
+            }
+
+            ftp.logout();
+            ftp.disconnect();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+	
+	
+	private static void downloadAllFile(FTPClient ftp,String remotePath,String localPath) {
+		try {
+			
+			FTPFile[] ftpFiles = ftp.listFiles();
+			downloadFile(localPath, ftp, ftpFiles);
+			
+			FTPFile[] directoryList = ftp.listDirectories();
+			 for (FTPFile directory : directoryList) {
+				 String newDirectory = directory.getName();
+				 // crate directory
+				 String newRemotePath = remotePath +"/"+ newDirectory;
+				 File subLocal = new File(localPath+"\\"+newDirectory);
+            	if(subLocal.mkdirs()) {
+            		// download
+            		ftp.changeWorkingDirectory(newRemotePath);
+            		System.out.println("Change remote path " + newRemotePath);
+   				 	downloadAllFile(ftp,newRemotePath,subLocal.toString());
+            	}
+			 }
+			 
+		} catch (IOException e) {
+		
+			e.printStackTrace();
+		}
+			
+		
+	}
+	
+
+	private static void downloadFile(String directory, FTPClient ftp, FTPFile[] ftpFiles)
+			throws FileNotFoundException, IOException {
+		if (ftpFiles != null && ftpFiles.length > 0) {
+		    //loop thru files
+		    for (FTPFile file : ftpFiles) {
+		        if (!file.isFile()) {
+		            continue;
+		        }
+		        System.out.println("File downlaod to " + directory +"\\"+ file.getName());
+		        //get output stream
+		        OutputStream output;
+		        output = new FileOutputStream(directory  +"\\"+ file.getName());
+		        //get the file from the remote system
+		        ftp.retrieveFile(file.getName(), output);
+		        //close output stream
+		        output.close();
+		    }
 		}
 	}
 	
