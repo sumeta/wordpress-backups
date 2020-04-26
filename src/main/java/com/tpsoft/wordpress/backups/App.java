37da832355ebba90f2bfbba419d3e7f0c9ef69db
tpsumeta <tpsumeta@gmail.com>
2020-04-26 16:01:40 +0700

    init

---
+++ src/main/java/com/tpsoft/wordpress/backups/App.java
@@ -0,0 +1,13 @@
+package com.tpsoft.wordpress.backups;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
